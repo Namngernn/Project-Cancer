95928f0112f86d4dc1e738f36f0aa25d6d912742
Aphisara Sonthi <nam.apsr.js@gmail.com>
2024-08-11 18:37:00 +0700

    Update config.js

--- project_backend-main/config.js
+++ project_backend-main/config.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: 'Namapsr4745',
+    password: '',
     database: 'cancer',
     waitForConnections: true,
     connectionLimit: 10,
